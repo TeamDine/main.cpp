a1dee508fda5da839095e28301dd3dcdf3dad94b
AlexAlvrzCastell <39219220+AlexAlvrzCastell@users.noreply.github.com>
2018-05-12 12:35:24 -0500

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -1,11 +1,12 @@
 /// main.cpp
 
 #include <iostream>
+#include "menu.h"           ///Clase menu principal
 
 using namespace std;
 
-int main()
-{
-    cout << "Que le Baga Berga Cñora!" << endl;
+int main(){
+    Menu enter;             ///Crea objeto tipo Menu 
+    enter.startMenu();      ///Entrada a Menu principal
     return 0;
 }
